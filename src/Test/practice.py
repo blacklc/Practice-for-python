d2758fd61962166a6874b38f51f8b8dd72c6a383
blacklc <575784862@qq.com>
2017-02-07 07:58:42 +0800

    Add pracitce
    Add fuction:socre(**socres)
    ;leapYear(year);get_coin(money);check_negative_sign(equation_list);calcu
    lator(equation)

--- src/Test/practice.py
+++ src/Test/practice.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 #coding:utf-8
 
+import math
+import re
+from types import IntType,LongType,FloatType,ComplexType
+
 if -1<0:
     print (1 + 2 * 4)
 else:
@@ -99,6 +103,203 @@ if middle:
     print '从小到大:',max,middle,min
 else:
     print '3个数相等',num,num,num
+
+for eachnum in (.2,.7):
+    print 'int(%.1f)\t%+.2f' % (eachnum,float(int(eachnum)))
+    print 'floor(%.1f)\t%+.2f' % (eachnum,math.floor(eachnum))
+    print 'round(%.1f)\t%+.2f' % (eachnum,round(eachnum))
+
+a=hex(255)
+print a,type(a),a*1
+
+class c:
+    @classmethod
+    def __nonzero__(self):
+        return False
+a=c()
+print bool(c)
+print bool(a)
+print c.__nonzero__()
+
+def socre(**socres):
+    """
+    根据分数返回评分成绩(A-F)
+    
+    :param  **sorcres
+            分数字典：key-学生姓名;value-学生成绩
+    :type   map
+    
+    :return 生成器;返回每个学生的评分成绩字典:key-学生姓名;value-评分成绩
+    """
+    for name in socres:
+        if 0 <= socres[name] < 60:
+            yield {name:'F'}
+        elif 60 <= socres[name] <= 69:
+            yield {name:'D'}
+        elif 70 <= socres[name] <= 79:
+            yield {name:'C'}
+        elif 80 <= socres[name] <= 89:
+            yield {name:'B'}
+        elif 90 <= socres[name] <= 100:
+            yield {name:'A'}
+        else:
+            yield {name:'error_socre'}
+
+sc={
+    'stu1':60,
+    'stu2':80,
+    'stu3':99,
+    'lc':100,
+    'zz':250,
+    'maomao':-1
+}
+for mark in socre(**sc):
+    for student in mark:
+        print student+':'+mark[student]
+
+def leapYear(year):
+    """
+    判断给定年份是否为闰年
+    
+    :param  year
+            给定年份
+    :type   int
+    
+    :return boolean True-是闰年;False-不是闰年
+    """
+    if (year % 4 == 0) and (year % 100 != 0):
+        return True
+    elif year % 400 == ］0:
+        return True
+    else:
+        return False
+
+print leapYear(2002)
+
+def get_coin(money):
+    """
+    计算给定金钱数量可兑换成硬币的最少数量
+    
+    :param  money
+            需兑换的金钱面额
+    :type   float
+    
+    :return 兑换信息字典:key-硬币面值与硬币总量sum;value-硬币个数
+    """
+    num_25,remainder = divmod(money*100,25)
+    num_10,remainder = divmod(remainder,10)
+    num_5,remainder = divmod(remainder,5)  
+    return {
+            25:num_25,
+            10:num_10,
+            5:num_5,
+            1:remainder,
+            'sum':remainder+num_5+num_10+num_25,
+            } 
+
+print get_coin(0.99)
+        
+def check_negative_sign(equation_list):
+    """
+    检查数字前的负号数量
+    
+    :param  equation_list
+            算式列表
+    :type   list
+    
+    :return element 计算操作数
+    """
+    negative_sign=0 #负号数量标记
+    element = equation_list.pop(0)
+    #检查数字前是否有负号
+    while element in ['+','-','*','/','**','//']:
+        if element not in ['+','-']:
+            return False
+        elif element == '-':
+            negative_sign += 1
+        element = equation_list.pop(0)
+    try:
+        element = float(element)
+        print element
+        if negative_sign % 2 == 0:
+            return element
+        else:
+            return -element
+    except:
+        raise "The formula is not standardized"
+    
+
+def calculator(equation):
+    """
+    简单计算器实现
+    
+    :param  equation
+            需计算的算式
+    :type   string
+    
+    :return result 返回运算结果
+    """
+    equation_list = re.split(r'(\+|-|\*\*|//|\*|/|%)', equation) #re.split可以支持同时使用多个切割符，并且如果对切割符加上括号则返回的list中也会加上切割符;实际上切割符部分是正则表达式；因此需要注意写法
+    #删除多余空白
+    for i,elem in enumerate(equation_list):
+        if elem == '':
+            equation_list.pop(i)
+    num1 = check_negative_sign(equation_list) #获取第一个操作数，并检查数字前是否有负号
+    opreation = equation_list.pop(0) #获取操作符
+    num2 = check_negative_sign(equation_list) #获取第二个操作数，并检查数字前是否有负号
+    #计算结果
+    if opreation == '+':
+        result = num1 + num2
+        return result
+    elif opreation == '-':
+        result = num1 - num2
+        return result
+    elif opreation == '*':
+        result = num1 * num2
+        return result
+    elif opreation == '/':
+        result = num1 / num2
+        return result
+    elif opreation == '//':
+        result = num1 // num2
+        return result
+    else:
+        result = num1 ** num2
+        return result
+
+result = calculator('--2**------3')
+print result
+
+result = eval("---1+2") #上述计算器功能可直接使用eval函数实现:将字符串str当成有效的表达式来求值并返回计算结果
+print result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
