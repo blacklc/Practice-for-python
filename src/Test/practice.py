dfd57a453b6c3b56888ad899b365a0f0e6b1917e
blacklc <575784862@qq.com>
2017-02-08 09:33:05 +0800

    add practice fuction

--- src/Test/practice.py
+++ src/Test/practice.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 #coding:utf-8
 
+from __future__ import division
+
 import math
 import re
+import random
+import sys
+
 from types import IntType,LongType,FloatType,ComplexType
 
 if -1<0:
@@ -169,7 +174,7 @@ def leapYear(year):
     """
     if (year % 4 == 0) and (year % 100 != 0):
         return True
-    elif year % 400 == ］0:
+    elif year % 400 == 0:
         return True
     else:
         return False
@@ -273,30 +278,162 @@ print result
 result = eval("---1+2") #上述计算器功能可直接使用eval函数实现:将字符串str当成有效的表达式来求值并返回计算结果
 print result
 
+def divide(num1,num2):
+    """
+    判断两个整型之间的整出关系
+    
+    :param  num1
+            整型数1
+    :type   int
+    :param  num2
+            整型数2
+    :type   int
+    
+    :return boolean True为两数之间存在整除关系；False为不存在整除关系
+    """
+    if num1 % num2:
+        return True
+    elif num2 % num1:
+        return True
+    else:
+        return False
 
+print sys.maxint #输出当前环境支持的最大整型数
+print -sys.maxint-1 #输出当前环境支持的最小整型数
+print sys.float_info #输出当前环境支持的浮点型信息
+print sys.long_info #输出当前环境支持的长整型信息
 
+def hour_to_min(time_string):
+    """
+    将小时分钟显示的时间转换成分钟显示
+    
+    :param  time_string
+            时间字符串
+    :type   string
+    
+    :return min 分钟表示的时间
+    """
+    timestr_list = time_string.split(':')
+    timestr_list = [int(time) for time in timestr_list]
+    min = timestr_list[0]*60 + timestr_list[1]
+    return min
 
+min=hour_to_min("11:10")
+print min
 
+def Gcd(num1,num2):
+    """
+    求两个数的最大公约数(辗转相除法)
+    
+    :param  num1
+            数值1
+    :type   int
+    :param  num2
+            数值2
+    :type   int
+    
+    :return gcd 最大公约数
+    """
+    while num2:
+        r = num1 % num2
+        (num1,num2) = (num2,r) #辗转相除
+    return num1
 
+def Lcm(num1,num2):
+    """
+    求两个数的最小公倍数(最小公倍数＝两数乘积／最大公约数)
+    
+    :param  num1
+            数值1
+    :type   int
+    :param  num2
+            数值2
+    :type   int
+    
+    :return lcm 最小公倍数
+    """
+    return (num1 * num2) // Gcd(num1, num2)
 
+g = Gcd(140, 21)
+print g
 
+l = Lcm(15,6)
+print l
 
+def payment(balance,paid):
+    """
+    计算家庭财务
+    
+    :param  balance
+            起始金额
+    :type   float
+    :param  paid
+            每月支出金额
+    :type   float
+    
+    :return 
+    """
+    print 'Pymt#','Pald','Balance'
+    print '-----','------','---------'
+    mouth = 0
+    while balance >= paid:
+        if mouth == 0:
+            print mouth,'$',0.00,'$',balance
+        else:
+            balance = balance - paid
+            print mouth,'$',paid,'$',balance
+        mouth += 1
+    print mouth,'$',balance,'$',0.00
 
+payment(100.00, 16.13)
 
+def random_list(rand_list):
+    """
+    按指定要求生成随机数列表
+    
+    :param  rand_list
+            随机数队列
+    :type   list
+    
+    :return None
+    """
+    #list_len = random.randint(2,100) #randint的范围为左闭右闭
+    while len(rand_list) <= random.randint(2,100):
+        rand_list.append(random.uniform(0,(2**31)-1))
 
+def getElement_from_RandomList(random_list):
+    """
+    按指定要求从随机数队列中取出子集并排序
+    
+    :param  random_list
+            随机数队列
+    :type   list
+    
+    :return child_list 返回子集
+    """
+    #childList_len = random.randint(1,100)
+    chlid_list = [ random_list[random.randrange(len(random_list))] for n in range(random.randint(1,100))]
+    chlid_list.sort()
+    return chlid_list
 
+rand_list = []
+random_list(rand_list)
+childlist = getElement_from_RandomList(rand_list)
+print rand_list
+print childlist
 
+print childlist * 2
 
+print rand_list[1:4] #切片是左闭右开，切片的范围是用序列索引描述的
 
+mlist = range(-5,-1)
+mlist.insert(0,None) #注意list的大部分方法返回的都是None;即方法是对原list进行操作，方法本身并不返回list
+print mlist
 
-
-
-
-
-
-
-
-
+s = 'abcde'
+i = -1
+for i in range(-1,-len(s),-1):
+    print s[:i],i
 
 
 
